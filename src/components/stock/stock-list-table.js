d2f90e3bb1924650b14feb8f175551e048a6eea4
Hakan Yilmaz <hakanbien@gmail.com>
2024-03-25 07:54:21 +0100

    25Martv2

--- src/components/stock/stock-list-table.js
+++ src/components/stock/stock-list-table.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Button, Form, InputGroup, Table } from 'react-bootstrap';
+import { Button, Form, Table, Modal } from 'react-bootstrap';
 import accessoires from './data/accessoires.json';
 import divers from './data/divers.json'
 import electromenagers from './data/electromenagers.json'
@@ -15,25 +15,159 @@ const StockListTable = () => {
     const allData = [...mobilier, ...accessoires, ...divers, ...electromenagers, ...sanitaires, ...surfaces];
     const [searchText, setSearchText] = useState("");
     const [records, setRecords] = useState(allData);
+    const [showModal, setShowModal] = useState(false);
+    const [newProduct, setNewProduct] = useState({
+      name: '',
+      description: '',
+      quantity: '',
+      note: '',
+      category: '',
+    });
+    const [errors, setErrors] = useState({});
 
- 
+    useEffect(() => {
+        const arr = allData.filter((item) =>
+            item.name.toLowerCase().includes(searchText.toLowerCase()) ||
+            item.description?.toLowerCase().includes(searchText.toLowerCase()) ||
+            item.quantity.toString().includes(searchText) ||
+            item.note?.toLowerCase().includes(searchText.toLowerCase()) ||
+            item.category?.toLowerCase().includes(searchText.toLowerCase())
+        );
+        setRecords(arr);
+        console.log("Filtered records:", arr); // Filtrelenmiş kayıtları logla
+    }, [searchText]);
+    
+    const handleDelete = (id) => {
+        const updatedRecords = records.filter(item => item.id !== id);
+        setRecords(updatedRecords); // Güncellenmiş listeyi state'e kaydet
+    };
 
-useEffect(() => {
-  const arr = allData.filter((item)=>
-    item.name.toLocaleLowerCase().includes(searchText.toLocaleLowerCase()) ||
-    item.label.toLocaleLowerCase().includes(searchText.toLocaleLowerCase()) ||
-    item.value.toLocaleLowerCase().includes(searchText.toLocaleLowerCase()) ||
-    item.note.toLocaleLowerCase().includes(searchText.toLocaleLowerCase()) ||
-    item.quantity.toString().includes(searchText)
-  );
-  setRecords(arr);
-}, [searchText]);
 
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        // Quantity için özel kontrol
+        if (name === "quantity") {
+          // Sadece sayısal değerler kabul edilir
+          if (!isNaN(value) || value === "") {
+            setNewProduct({ ...newProduct, [name]: value });
+          }
+        } else {
+          setNewProduct({ ...newProduct, [name]: value });
+        }
+      };
+      
+    const validateForm = () => {
+      const errors = {};
+      if (!newProduct.name) errors.name = 'Name is required';
+      if (!newProduct.quantity) errors.quantity = 'Quantity is required';
+      if (!newProduct.category) errors.category = 'Category is required';
+      else if (isNaN(newProduct.quantity)) errors.quantity = 'Quantity must be a number';
+      return errors;
+    };
 
-
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const formErrors = validateForm();
+        console.log("Form Errors:", formErrors);
+        if (Object.keys(formErrors).length === 0) {
+            const newProductWithId = { ...newProduct, id: records.length + 1 };
+            const newRecords = [...records, newProductWithId];
+            setRecords(newRecords); // Kayıtları güncelle
+            console.log("New Records after addition:", newRecords);
+            setShowModal(false);
+            setNewProduct({ name: '', description: '', quantity: '', note: '', category: '' });
+            if(searchText === ""){
+                // Arama metni boşsa, filtreleme yapılmadan tüm kayıtları göster
+                setRecords(newRecords);
+            }
+        } else {
+            setErrors(formErrors);
+        }
+    };
+    
+    
+      
 
     return (
       <div className="stock-list-table-wrapper">
+        <Button variant="success" onClick={() => setShowModal(true)}>Add New Product</Button>
+
+        <Modal show={showModal} onHide={() => setShowModal(false)}>
+          <Modal.Header closeButton>
+            <Modal.Title>Add New Product</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form>
+              <Form.Group className="mb-3">
+                <Form.Label>Name</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="name"
+                  value={newProduct.name}
+                  onChange={handleInputChange}
+                  isInvalid={!!errors.name}
+                />
+                <Form.Control.Feedback type="invalid">
+                  {errors.name}
+                </Form.Control.Feedback>
+              </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Label>Description</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="description"
+                  value={newProduct.description}
+                  onChange={handleInputChange}
+                />
+              </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Label>Quantity</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="quantity"
+                  value={newProduct.quantity}
+                  onChange={handleInputChange}
+                  isInvalid={!!errors.quantity}
+                />
+                <Form.Control.Feedback type="invalid">
+                  {errors.quantity}
+                </Form.Control.Feedback>
+              </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Label>Note</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="note"
+                  value={newProduct.note}
+                  onChange={handleInputChange}
+                />
+              </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Label>Category</Form.Label>
+                <Form.Select
+                  name="category"
+                  value={newProduct.category}
+                  onChange={handleInputChange}
+                  isInvalid={!!errors.category}
+                >
+                  <option value="">Select a category</option>
+                  {["Mobilier", "Accessoires", "Divers", "Électromenagers", "Sanitaires", "Surfaces"].map((category) => (
+                    <option key={category} value={category}>{category}</option>
+                  ))}
+                </Form.Select>
+                <Form.Control.Feedback type="invalid">
+                  {errors.category}
+                </Form.Control.Feedback>
+              </Form.Group>
+              <div className="button-group-new-product">
+              <Button variant="primary" onClick={handleSubmit}>Add</Button>
+              <Button variant="danger" onClick={() => setShowModal(false)}>Cancel</Button>
+              </div>
+              
+            </Form>
+          </Modal.Body>
+        </Modal>
+
       
 
         <div className="data-filter">
@@ -72,7 +206,7 @@ useEffect(() => {
                     </Button>
                   </td>
                   <td>
-                    <Button variant="outline-danger">
+                    <Button variant="outline-danger" onClick={() => handleDelete(item.id)}>
                       <AiTwotoneDelete />
                     </Button>
                   </td>
