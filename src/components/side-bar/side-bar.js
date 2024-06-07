fd7034c50417cf022b9c6603a41f2ac48680f19f
Hakan Yilmaz <hakanbien@gmail.com>
2024-06-08 00:27:20 +0200

    8june-v01

--- src/components/side-bar/side-bar.js
+++ src/components/side-bar/side-bar.js
@@ -1,9 +1,8 @@
 import React from "react";
-import { Link, useLocation, useNavigate } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import logo from "../../assets/img/logo-dalyana.png"
 import "./side-bar.css";
 import {
-  RiHome3Line,
   RiLogoutCircleRLine,
   RiDashboardLine,
 } from "react-icons/ri";
@@ -12,32 +11,13 @@ import { IoMdContacts } from 'react-icons/io';
 import { BsKanban, BsBarChartFill, BsBoxSeamFill } from 'react-icons/bs';
 import { FaNetworkWired } from "react-icons/fa";
 
-/* import { useStore } from "../../../store";
-import alertify from "alertifyjs";
-import { logout } from "../../../store/user/userActions"; */
 import { Container, Dropdown, Nav, Navbar } from "react-bootstrap";
+import { useAuth } from '../../../src/context/AuthContext';
 
 const SideBar = () => {
-  /* const { userState, dispatchUser } = useStore();
-  const navigate = useNavigate();
-
-  const handleLogout = () => {
-    alertify.confirm(
-      "Logout",
-      "Are you sure want to logout?",
-      () => {
-        dispatchUser(logout());
-        localStorage.removeItem("token");
-        navigate("/");
-      },
-      () => {
-        console.log("canceled");
-      }
-    );
-  };
- */
   const location = useLocation();
   const currentPath = location.pathname;
+  const { userRole } = useAuth(); // AuthContext'ten userRole'u alın
 
   return (
     <Navbar expand="lg" className="admin-navbar" variant="dark">
@@ -49,33 +29,30 @@ const SideBar = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse className="navbar-menu" id="basic-navbar-nav">
           <Nav className="me-auto">
-           {/*  <Nav.Link as={Link} to="/" className={currentPath === "/" ? "active" : ""}>
-              <RiHome3Line /> Accueil
-            </Nav.Link>  */}
-            
             <Nav.Link as={Link} to="/" className={currentPath === "/" ? "active" : ""}>
-            <IoMdContacts /> Clients
+              <IoMdContacts /> Clients
             </Nav.Link>
-            
             
             <Nav.Link as={Link} to="/projets" className={currentPath === "/projets" ? "active" : ""}>
               <AiFillFolderOpen /> Projets
             </Nav.Link>
 
-
-            
-         
             <Nav.Link as={Link} to="/elements-produits" className={currentPath === "/elements-produits" ? "active" : ""}>
               <RiDashboardLine /> Elements & Produits
             </Nav.Link>
             <Nav.Link as={Link} to="/stock" className={currentPath === "/stock" ? "active" : ""}>
-            <BsBoxSeamFill /> Stock
+              <BsBoxSeamFill /> Stock
             </Nav.Link>
-            <Nav.Link as={Link} to="/factures" className={currentPath === "/factures" ? "active" : ""}>
-              <AiFillEdit /> Factures
-            </Nav.Link>
+
+            {/* userRole 'admin' ise Factures linkini göster */}
+            {userRole === 'admin' && (
+              <Nav.Link as={Link} to="/factures" className={currentPath === "/factures" ? "active" : ""}>
+                <AiFillEdit /> Factures
+              </Nav.Link>
+            )}
+
             <Nav.Link as={Link} to="/employees" className={currentPath === "/employees" ? "active" : ""}>
-            <FaNetworkWired /> Employees
+              <FaNetworkWired /> Employees
             </Nav.Link>
             <Nav.Link as={Link} to="/calendrier" className={currentPath === "/calendrier" ? "active" : ""}>
               <AiOutlineCalendar /> Calendrier
@@ -83,11 +60,15 @@ const SideBar = () => {
             <Nav.Link as={Link} to="/missions" className={currentPath === "/missions" ? "active" : ""}>
               <BsKanban /> Missions
             </Nav.Link>
-            <Nav.Link as={Link} to="/statistiques" className={currentPath === "/statistiques" ? "active" : ""}>
-              <BsBarChartFill /> Statistiques
-            </Nav.Link>
-            {/* <Nav.Link onClick={handleLogout}> ALTTAKI YOKTU, SADECE HATA VERMEMESI ICIN BUNUN YERINE KOYDUM ,BIR ALTTAKI SILINECEK */}
-            <Nav.Link >
+
+            {/* userRole 'admin' ise Statistiques linkini göster */}
+            {userRole === 'admin' && (
+              <Nav.Link as={Link} to="/statistiques" className={currentPath === "/statistiques" ? "active" : ""}>
+                <BsBarChartFill /> Statistiques
+              </Nav.Link>
+            )}
+
+            <Nav.Link>
               <RiLogoutCircleRLine /> Logout
             </Nav.Link>
           </Nav>
